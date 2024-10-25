dcd11aa7c5412167c02192bad586a22e7ebf9c1c
Robert Vilcu <robertvilcu89@gmail.com>
2024-10-25 17:01:55 +0100

    chore: changed dev options for server proxy

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,24 @@ const devConfig = {
         port: 3000,
         cors: true,
         proxy: {
-            "/api": {
-                target: "http://localhost:5001",
+            '/api': {
+                target: 'http://localhost:5001',
                 changeOrigin: true,
                 secure: false,
-                rewrite: (path) => path.replace(/^\/api/, ""),
+                ws: true,
+                configure: (proxy, _options) => {
+                    proxy.on('error', (err, _req, _res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq, req, _res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, _res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
             }
-        },
+        }
     },
 };
 
